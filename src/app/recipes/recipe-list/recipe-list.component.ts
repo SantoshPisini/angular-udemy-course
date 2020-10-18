e8222552851fe73f6a5852e7927e5c1e99d71365
Santosh <santoshsrisai36@gmail.com>
2020-10-18 15:35:58 +0530

    Some more progress

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipies: Recipe[] = [
+    new Recipe('Breakfast Pizzas', 'Pizza for breakfast? We\'re in !These individual pizzas are inspired by Mexican flavors, but you can easily mix it up by using other ingredients like your favorite proteins and veggies.', 'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2018/12/healthy-breakfast-pizzas.jpg'),
+    new Recipe('Breakfast Pizzas', 'Pizza for breakfast? other ingredients.', 'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2018/12/healthy-breakfast-pizzas.jpg'),
+
+  ];
 
   constructor() { }
 
